7f7ee016976fb9d12950957c0b512b9060c25001
Scott Malin <mrsemalin@gmail.com>
2021-03-06 21:18:43 -0700

    prove09 basic requirements

---
+++ controllers/proveAssignments/prove09/prove09.js
@@ -0,0 +1,16 @@
+function displayZone(answer) {
+
+
+
+   if (answer == "service_retail") { // Show div if selected
+     document.getElementById("is_package").style.display = "block";
+
+
+   } else {
+
+     document.getElementById('is_package').style.display = "none";
+
+   }
+
+ }
+
--- routes/proveRoutes/prove09.js
+++ routes/proveRoutes/prove09.js
@@ -2,11 +2,60 @@
 const express = require('express');
 const router = express.Router();
 const home = '/proveAssignments/09';
+const url = require('url');
 
-router.get('/',(req, res, next) => {
-    res.render('pages/proveAssignments/prove09', { 
-        title: 'Prove Assignment 09', 
+router.get('/', (req, res, next) => {
+    res.render('pages/proveAssignments/prove09/prove09', {
+        title: 'Prove Assignment 09',
         path: '/prove09', //  EJS 
+    });
+});
+
+router.get('/postal-rate', (req, res, next) => {
+    const queryObject = url.parse(req.url, true).query;
+    console.log(queryObject);
+    let weight = queryObject.weight;
+    let postage = queryObject.postage;
+    let zone = null;
+    var cost = 0.00;
+    for (let i = 0; i <= weight; i++) {
+        cost += .20;
+        //console.log("Cost: " + cost);
+    }
+    switch (postage) {
+        case "letter_stamped":
+            cost += .15;
+            //console.log("Cost: " + cost);
+            postage = "Letter Stamped";
+            break;
+        case "letter_metered":
+            cost += .31;
+            postage = "Letter Metered";
+            break;
+        case "envelope":
+            cost += .80;
+            postage = "Large Envelopes";
+            break;
+        case "service_retail":
+            cost += 3.80;
+            postage = "First-Class Package Service—Retail";
+            break;
+        default:
+            cost += .20;
+            break;
+
+    }
+    
+    if (req.query.zone != null) {
+        zone = req.query.zone;
+    }
+    res.render('pages/proveAssignments/prove09/prove09-response.ejs', {
+        title: 'Prove Assignment 09',
+        path: '/prove09/postal-rate', //  EJS 
+        weight: weight,
+        postage: postage,
+        zone: zone,
+        cost: cost,
     });
 });
 
